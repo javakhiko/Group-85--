fee9be336c8a4781e120a9ed405a11aefa0fd62e
javakhiko <javaxishvili.andri@gmail.com>
2025-11-21 21:17:53 +0400

    “commit”

---
+++ day16/homework/hw1.py
@@ -0,0 +1,10 @@
+# 1)გამოიტანეთ თქვენი სახელი 10 ჯერ forloop/while loop ორივეთი
+
+for i in range(10):
+    print('andria')
+i=0
+
+while i < 10:
+    print('andria')
+    
+    i=i + 1
---
+++ day16/homework/hw9.py
@@ -0,0 +1,25 @@
+# 9)შექმენით კალკულატორი როგორიც ჩვენ გავაკეთეთ,
+# დაუმატეთ სხვა მათემატიკური ოპერატორები,ასევე დაუმატეთ შედარების ოპერატორებიც
+
+print("########## CALCULATOR ##########")
+
+while True:
+    num1 = int(input('Enter First Number. Type "exit" to quit: '))
+    operator = input('Enter Operator (+,-,/,*). (type "exit" to quit): ')
+    
+    if operator== "exit":
+        print("Calculator Closed.")
+        break
+
+    num2 = int(input('Enter Second Number: '))
+
+    if operator == '+':
+        print('Result:', num1 + num2)
+    elif operator == '-':
+        print('Result:', num1 - num2)
+    elif operator == '/':
+        print('Result:', num1 / num2)
+    elif operator == '*':
+        print('Result:', num1 * num2)
+    else:
+        print('The operator you typed isnt the correct operator')
