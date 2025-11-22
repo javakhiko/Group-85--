6dbbd2b1cf0b263dbeceff3b4f5a7dfb273a8044
javakhiko <javaxishvili.andri@gmail.com>
2025-11-22 15:29:35 +0400

    “commit”

--- day14/homework/hw2.py
+++ day14/homework/hw2.py
@@ -1,11 +1,10 @@
 i=200
 
 while i < 400: 
-    i=i+1
     if i%5==0:
         print(i)
     elif i%4==0:
         print(i)
-
+    i=i+1
 
 
--- day14/homework/hw4.py
+++ day14/homework/hw4.py
@@ -5,3 +5,4 @@ i=0
 while i<number:
     print(number)
     number=number-1
+    i=i+1
