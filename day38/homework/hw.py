baf4df9aa3c81f49c8dd153bdbd2c034c4a8cc6a
javakhiko <javaxishvili.andri@gmail.com>
2026-02-17 18:36:40 +0400

    “commit”

---
+++ day38/homework/hw.py
@@ -0,0 +1,32 @@
+# codewars 1
+
+# def repeat_str(repeat, string):
+#     return repeat * string
+
+# codewars 2
+
+# def getCount(inputStr):
+#     count = 0
+#     for i in inputStr:
+#         if i in "aeiouAEIOU":
+#            count = count + 1
+#     return count
+
+# codewars 3
+
+# def positive_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         if i > 0:
+#             sum = sum + i
+#     return sum
+
+# codewars 4
+
+# def string_to_number(s):
+#     return int(s)
+
+# codewars 5
+
+# def greet()
+#     return 'hello world!'
