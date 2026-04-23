4b407a2e5c1aff636ec6a15c49645c887e9f3e2e
javakhiko <javaxishvili.andri@gmail.com>
2026-04-23 17:33:43 +0400

    “commit”

--- day57/homework/hw.py
+++ day57/homework/hw.py
@@ -26,3 +26,69 @@
 
 #   codewars 3
 
+# ?
+#   codewars 4
+# def to_float_array(arr): 
+#     result = []
+    
+#     for i in arr:
+#         if '.' in i:
+#             result.append(float(i))
+#         else:
+#             result.append(int(i))
+    
+#     return result
+
+#   codewars 5
+
+# def capitalize(s):
+#     even = ""
+#     odd = ""
+    
+#     for i in range(len(s)):
+#         if i % 2 == 0:
+#             even += s[i].upper()
+#             odd += s[i]
+#         else:
+#             even += s[i]
+#             odd += s[i].upper()
+    
+#     return [even, odd]
+
+#   codewars 6
+
+# def adjacent_element_product(array):
+#     max = array[0] * array[1]
+    
+#     for i in range(len(array) - 1):
+#         current = array[i] * array[i + 1]
+#         if current > max:
+#             max = current
+    
+#     return max
+
+#   codewars 7
+
+# def show_sequence(n):
+#     if n < 0:
+#         return str(n) + "<0"
+#     elif n==0:
+#         return str(n) + '=0'
+    
+#     s = 0
+#     result = ""
+    
+#     for i in range(n + 1):
+#         s += i
+#         result += str(i)
+#         if i < n:
+#             result += "+"
+    
+#     return result + " = " + str(s)    
+# 
+# # codewars 8
+# 
+#   codewars 9
+# 
+#   codewars 10
+#   
