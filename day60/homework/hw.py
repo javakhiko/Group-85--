bdeb397028fb0e9537832d4df2fb4432df484406
javakhiko <javaxishvili.andri@gmail.com>
2026-04-28 17:49:11 +0400

    “commit”

---
+++ day60/homework/hw.py
@@ -0,0 +1,27 @@
+#   codewars 1
+
+
+#   codewars 2
+
+# def compute_sum(n):
+#     total = 0
+#     for i in range(1, n + 1):
+#         for digit in str(i):
+#             total += int(digit)
+#     return total
+
+#   codewars 3
+
+#   codewars 4
+
+# def digital_root(n):
+#     total = 0
+    
+#     for i in range(1, n + 1):
+#         for x in str(i):
+#             total += int(x)
+    
+#     return total
+
+#   codewars 5
+
