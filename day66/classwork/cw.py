fa098d4c7e299f9894cd370c4f67b9e6dda5d651
javakhiko <javaxishvili.andri@gmail.com>
2026-05-19 20:28:45 +0400

    “commit”

---
+++ day66/classwork/cw.py
@@ -0,0 +1,7 @@
+# def longest(a1, a2):
+#     a=a1+a2
+#     b=''
+#     for i in a:
+#         if i not in b:
+#             b+=i
+#     return ''.join(sorted(b))
---
+++ day66/homework/hw.py
@@ -0,0 +1,30 @@
+#   codewars 1
+# def add(num1, num2):
+#     str1 = str(num1)
+#     str2 = str(num2)
+    
+#     while len(str1) < len(str2):
+#         str1 = "0" + str1
+#     while len(str2) < len(str1):
+#         str2 = "0" + str2
+#     result_str = ""
+#     for i in range(len(str1)):
+#         column_sum = int(str1[i]) + int(str2[i])
+#         result_str += str(column_sum)
+        
+#     return int(result_str)
+
+#   codewars2
+# ?
+
+#   codewars 3
+# def nth_smallest(arr, n):
+#     return sorted(arr)[n - 1]
+
+# print(nth_smallest([2, 1, 3, 3, 1, 2], 3))
+
+#   codewars 4
+# ?
+
+#   codewars 5
+# ?
