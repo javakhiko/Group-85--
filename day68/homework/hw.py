cac0747c86608b816c409eb539e1ff550f9d88da
javakhiko <javaxishvili.andri@gmail.com>
2026-05-26 20:34:10 +0400

    “commit”

---
+++ day68/homework/hw.py
@@ -0,0 +1,24 @@
+#   codewars 1
+# def reverse_words(text):
+#     return ' '.join(word[::-1] for word in text.split(' '))
+
+#   codewars 2
+# ?
+
+#   codewars 3
+# def is_pangram(st):
+#     a_z = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#     st = st.upper()
+
+#     for i in a_z:
+#         if i not in st:
+#             return False
+
+#     return True
+
+#   codewars 4
+# ?
+
+#   codewars 5
+
+
