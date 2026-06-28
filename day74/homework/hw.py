05f339c23de2d62fd0883b1908ed59c8d1eae6e1
javakhiko <javaxishvili.andri@gmail.com>
2026-06-28 16:40:53 +0400

    “commit”

---
+++ day74/homework/hw.py
@@ -0,0 +1,19 @@
+#   codewars 1
+
+# def break_chocolate(n, m):
+#     if n * m == 1 or n * m == 0:
+#         return 0
+#     return n * m - 1    
+
+#   codewars 2
+
+# def is_anagram(test, original):
+#     return sorted(test. lower()) == sorted(original. lower())
+
+#   codewars 3
+
+# def over_the_road(address, n):
+#     result = 2 * n + 1 - address
+#     return result
+
+#   codewars 4
